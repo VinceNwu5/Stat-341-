b4d56a3c8d2cd718859d338f7fc1faa57e613dd4
VinceNwu5 <vnwulu@claflin.edu>
2025-09-09 16:14:49 -0400

    Add files via upload

---
+++ correlation-test.R
@@ -0,0 +1,10 @@
+# Author: Vincent Nwulu
+# Date: 09/04/2005
+# Purpose:  Test the correlation
+
+if(!require(devtools)) install.packages("devtools"); devtools::install_github("kassambara/ggpubr")
+
+
+# Load the Library bggpubr
+Library(ggpubr)
+
